b0acbab160e608badf020cfcd271ba5a7b602ecd
Mohd Azfar <62957738+mo-azfar@users.noreply.github.com>
2020-05-22 09:23:20 +0800

    update

--- Custom/Kernel/Output/HTML/ITSMConfigItem/LayoutText.pm
+++ Custom/Kernel/Output/HTML/ITSMConfigItem/LayoutText.pm
@@ -1,18 +1,48 @@
+##################SNIP#################################
 
+sub InputCreate {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    for my $Argument (qw(Key Item)) {
+        if ( !$Param{$Argument} ) {
+            $Kernel::OM->Get('Kernel::System::Log')->Log(
+                Priority => 'error',
+                Message  => "Need $Argument!",
+            );
+            return;
+        }
+    }
+
+    my $Value = $Param{Value};
+    if ( !defined $Param{Value} ) {
+        $Value = $Param{Item}->{Input}->{ValueDefault} || '';
+    }
 
     my $Class    = '';
     my $Size     = 'W50pc';
     my $Required = $Param{Required};
     my $Invalid  = $Param{Invalid};
     my $ItemId   = $Param{ItemId};
-    
     ########ADD THIS CODE PART 1##################
     #### for readonly definition###########
     my $ReadOnly = $Param{ReadOnly};  
-    ########END CODE######################
-
-    ###-----------------SNIP---------------------#####
+    ########END CODE###################### 
     
+    if ($Required) {
+        $Class .= ' Validate_Required';
+    }
+
+    if ($Invalid) {
+        $Class .= ' ServerError';
+    }
+    $Class .= ' ' . $Size;
+    my $String = "<input type=\"text\" name=\"$Param{Key}\" class=\"$Class\" ";
+
+    if ($ItemId) {
+        $String .= "id=\"$ItemId\" ";
+    }
+
     if ($Value) {
 
         # get layout object
@@ -22,16 +52,31 @@
         if ( $Param{Item}->{Input}->{Translation} ) {
             $Value = $LayoutObject->{LanguageObject}->Translate($Value);
         }
-        
+
         ########ADD THIS CODE PART 2##################
         #### for readonly definition###########
         if ( $Param{Item}->{Input}->{ReadOnly} and $Value ne '' ) {
        	$String .= 'readonly="readonly" ';
         }
         ########END CODE########################
-
-        ###-----------------SNIP---------------------#####
         
+        # transform ascii to html
+        $Value = $LayoutObject->Ascii2Html(
+            Text           => $Value,
+            HTMLResultMode => 1,
+        );
     }
 
-  ###-----------------SNIP---------------------#####
+    $String .= "value=\"$Value\" ";
+
+    # add maximum length
+    if ( $Param{Item}->{Input}->{MaxLength} ) {
+        $String .= "maxlength=\"$Param{Item}->{Input}->{MaxLength}\" ";
+    }
+
+    $String .= '/> ';
+
+    return $String;
+}
+
+##################SNIP#################################
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # OTRS-Config-Item-Read-Only-Field
-Read only definition field for config item
+- Built for OTRS CE V6.0.x
+- Read only definition field for config item
+- This modifictaion will extend Config Item definition to has Read Only field.
+
+1. You are required to modify the existing LayoutText.pm file (do it at Custom directory please).
+
+2. At Config Item definiton, add 'ReadOnly: 1' parameter.
+		
+	Example: 
+	
+	- Input:
+    	MaxLength: 100
+    	Size: 50
+    	Type: Text
+    	ReadOnly: 1
+  	Key: SerialNumber
+  	Name: Serial Number
+  	Searchable: 1
+
+	[![Capture.png](https://i.postimg.cc/c13bn7hN/Capture.png)](https://postimg.cc/grYs9Z1M)
